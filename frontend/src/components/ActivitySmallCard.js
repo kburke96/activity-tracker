0dd87352b6475e2ddd6f80b299610793392dd063
Kevin Burke <kburke96@gmail.com>
2021-07-27 21:57:39 +0100

    Add some (bad) styling for activity cards

--- frontend/src/components/ActivitySmallCard.js
+++ frontend/src/components/ActivitySmallCard.js
@@ -1,18 +1,25 @@
 import { React } from 'react';
 import './ActivitySmallCard.scss';
 
-export const ActivitySmallCard = ({activity}) => {
+export const ActivitySmallCard = ({ activity }) => {
+    
+
     if (!activity) {
         return (
             <p>Activity not found.</p>
         );
     }
-        
+
+
+    const isActivityCycling = activity.activityType === "Cycling";
     return (
         <div className="ActivitySmallCard">
-            <p>{activity.activityType}</p>
-            <p>{activity.activityName}</p>
-            <p>{activity.time}</p>
+            <div className={isActivityCycling ? 'cycling' : 'not-cycling'}>
+
+                <p>{activity.activityType}</p>
+                <p>{activity.activityName}</p>
+                <p>{activity.time}</p>
+            </div>
         </div>
     );
 }
