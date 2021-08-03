99a6e2632b0ad01d91d839eb9ef14ca4369dfea1
Kevin Burke <kburke96@gmail.com>
2021-08-03 14:51:14 +0100

    Add Routing to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
-import {HashRouter as Router, Route, Switch} from 'react-router-dom';
-import { ActivityPage } from './pages/ActivityPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { ActivitiesPage } from './pages/ActivitiesPage';
+import 'leaflet/dist/leaflet.css';
+import React from 'react';
+import { SingleActivityPage } from './pages/SingleActivityPage';
 
 function App() {
+  // const App = () => {
   return (
     <div className="App">
-      <ActivityPage />
+      <Router>
+        <Switch>
+          <Route path="/activity/:activityId">
+            <SingleActivityPage />
+          </Route>
+          <Route exact path="/">
+            <ActivitiesPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
+
   );
 }
 
