790ffbd1d98bee12e290b9080e968b6f7b77eda3
Kevin Burke <kburke96@gmail.com>
2021-08-29 17:08:03 +0100

    Add changes to connect to Postgres db

--- src/main/java/com/kevin/activitytracker/Controllers/HomeController.java
+++ src/main/java/com/kevin/activitytracker/Controllers/HomeController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin()
 public class HomeController {
     
+    @Autowired
     private ActivityRepository activityRepository;
 
     
