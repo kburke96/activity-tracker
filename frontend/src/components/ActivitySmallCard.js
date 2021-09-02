7f1998dab3f4e59e241663cf44d8bd8c2742562c
Kevin Burke <kburke96@gmail.com>
2021-09-02 15:21:01 +0100

    Add activity images to ActivitySmallCard

--- frontend/src/components/ActivitySmallCard.js
+++ frontend/src/components/ActivitySmallCard.js
@@ -1,9 +1,10 @@
 import { React } from 'react';
 import './ActivitySmallCard.scss';
 import { Link } from 'react-router-dom';
+// import { running } from 'running.png';
 
 export const ActivitySmallCard = ({ activity }) => {
-    
+
 
     if (!activity) {
         return (
@@ -14,31 +15,58 @@ export const ActivitySmallCard = ({ activity }) => {
     var activityDateParts = activity.activityDate.split(" ");
     var activityDate = activityDateParts[0];
     const isActivityCycling = activity.activityType === "Cycling";
+    var logo = null;
+    switch (activity.activityType) {
+        case "Cycling":
+            logo = "cycling.png";
+            break;
+        case "Running":
+            logo = "running.png";
+            break;
+        case "Yoga":
+            logo = "yoga.png";
+            break;
+        case "Cardio":
+            logo = "cardio.png";
+            break;
+        case "Hiking":
+            logo = "hiking.png";
+            break;
+        default:
+            logo = null;
+            break;
+    }
+
+    
     return (
         <div className="ActivitySmallCard">
             <div className={isActivityCycling ? 'cycling' : 'not-cycling'}>
-                <table className="activity-table">
-                    <tr>
-                        <th>
-                            <p className="activity-type-name">{activity.activityType}</p>
-                        </th>
-                        <th>
-                            <p className="activity-distance">{activity.distance}km</p>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>
-                            <p className="activity-name"><Link to={{pathname: `/activity/${activity.id}`}}>{activity.activityName} - {activityDate}</Link></p>
-                        </th>
-                        <th>
-                            <p className="activity-time">{activity.time}</p>
-                        </th>
-                    </tr>
-                </table>
-                
-                
-                
+                <div className="logo">
+                    <img src={logo} width="50" height="50"></img>
+                </div>
+
+                    <table className="activity-table">
+                        <tr>
+                            <th>
+                                <p className="activity-type-name">{activity.activityType}</p>
+                            </th>
+                            <th>
+                                <p className="activity-distance">{activity.distance}km</p>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <p className="activity-name"><Link to={{ pathname: `/activity/${activity.id}` }}>{activity.activityName} - {activityDate}</Link></p>
+                            </th>
+                            <th>
+                                <p className="activity-time">{activity.time}</p>
+                            </th>
+                        </tr>
+                    </table>
+
+
+
+                </div>
             </div>
-        </div>
-    );
+            );
 }
